a09100484bde221dd772fa21e261b34fa33ca243
josephchiang <josephchiang@digiwin.com>
2020-10-27 15:13:49 +0800

    JSTest

---
+++ JS.js
@@ -0,0 +1,28 @@
+var json = {
+    "std_data": {
+      "execution": {
+        "code": "0",
+        "sql_code": "",
+        "description": ""
+      },
+      "parameter": {
+        "purchase_data": [
+          {
+            "purchase_no": "0001",
+            "purchase_seq": "0001A",
+            "purchase_sub_seq": "0001B",
+            "eoc_company_id": "雲端營運公司編號",
+            "eoc_site_id": "雲端營運據點編號",
+            "eoc_region_id": "雲端營運域編號"
+          }
+        ]
+      }
+    }
+  };
+
+  var json2 = [{name:'a',value:'001'},{name:'b',value:'b'}];
+
+  //console.log(json.std_data.parameter.purchase_data[0].purchase_no);
+  console.log(json2[0].name);
+
+  
